6c60a1c8c218c44c31f74e60f3f5b88004dcd393
Seb <sebastien.philippe5@gmail.com>
2019-11-26 18:39:07 +0100

    Ajout de la partie mon compte

--- screens/Account.js
+++ screens/Account.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { View,TouchableOpacity, StyleSheet,Text,TextInput,Button } from 'react-native';
+
 
 export default function Account() {
   return (
-    <ScrollView style={styles.container}>
-    </ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.textHeader}>Mon compte</Text>
+        <Text style={styles.textConnexion}>Connexion</Text>
+        <TouchableOpacity style={styles.container}>
+          <TextInput style={styles.textinput} placeholder='Identifiant'/>
+          <TextInput style={styles.textinput} placeholder='Mot de passe'/>
+          <Button style={styles.button}
+                  color='#ffffff'
+                  title='Se connecter' onPress={() => {}}/>
+        </TouchableOpacity>
+      </View>
   );
 }
 
@@ -18,4 +28,26 @@ const styles = StyleSheet.create({
     paddingTop: 15,
     backgroundColor: '#34aaa2',
   },
+  textHeader: {
+    fontSize:50,
+    color:'#ffffff',
+    textAlign:'center'
+  },
+  textConnexion: {
+    fontSize:30,
+    color:'#60D4B2',
+    textAlign:'center'
+  },
+  button: {
+    backgroundColor: '#e05a47',
+    fontSize:35,
+    color:'#ffffff'
+  },
+  textinput:{
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    color:'#ffffff',
+    fontSize:20
+  }
 });
