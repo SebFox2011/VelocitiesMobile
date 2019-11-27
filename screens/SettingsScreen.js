531c40e70474488f6a3ee5790ec1b6c872958a9c
Seb <sebastien.philippe5@gmail.com>
2019-11-27 16:52:36 +0100

    Ajout de la cartographie

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import {StyleSheet, Text} from "react-native";
+import React, {Component} from 'react';
+import {StyleSheet,Dimensions, Text,View} from "react-native";
+import MapView from 'react-native-maps'
 
-export default function SettingsScreen() {
-  return <Text style={styles.container}>TEXT</Text>;
+export default class SettingsScreen extends Component {
+  constructor() {
+    super();
+    this.state={
+      region: {
+        latitude: 48.0978255,
+        longitude: -1.6847536,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }
+    }
+  }
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+  render() {
+    return (
+        <View style={styles.container}>
+          <Text>Carte google maps</Text>
+          <MapView style={styles.mapStyle}
+              region={this.state.region}
+              onRegionChange={(region) => this.onRegionChange}
+          />
+        </View>
+    );
+  }
 }
 
 SettingsScreen.navigationOptions = {
@@ -13,5 +38,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#34aaa2',
+    alignItems:'center'
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width/1.1,
+    height: Dimensions.get('window').height/2,
+    marginLeft: 5,
+    marginRight: 5,
   }
 });
